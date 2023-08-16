5d9fa44625e28419062ec785f4d72aa2becac65b
Fatih Kabay <mfthkabay@gmail.com>
2023-08-16 23:28:39 +0300

    added sphere and plane geometry

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
 import './App.css'
 import Three from './components/three';
+import { Html } from '@react-three/drei';
 
 function App() {
   return (
     <Canvas id="three-canvas-container">
+
+      <Html>
+        <div>text</div>
+      </Html>
+
       <Suspense fallback={null}>
         <Three />        
       </Suspense>
--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,7 +1,18 @@
+/* eslint-disable react/no-unknown-property */
+// eslint-disable-next-line no-unused-vars
+import { angleToRadians } from "../../utils/angle"
+
 export default function Three() {
   return (
     <>
-
+     <mesh>
+      <sphereGeometry args={[1, 32, 32]} />
+      <meshStandardMaterial color="#ffffff" />
+     </mesh>
+     <mesh>
+      <planeGeometry args={[7, 7]} />
+      <meshStandardMaterial color="#ffffff" />
+     </mesh>
     </>
   )
 }
