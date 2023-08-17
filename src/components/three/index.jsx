8f6636729a18dca6b66483318e927d30cbe105e1
Fatih Kabay <mfthkabay@gmail.com>
2023-08-17 23:38:10 +0300

    added camera and floor rotation fixe

--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,17 +1,21 @@
 /* eslint-disable react/no-unknown-property */
 // eslint-disable-next-line no-unused-vars
+import { PerspectiveCamera } from "@react-three/drei"
 import { angleToRadians } from "../../utils/angle"
 
 export default function Three() {
   return (
     <>
+
+     <PerspectiveCamera makeDefault position={[0, 0, 3]}/>
+
      <mesh>
       <sphereGeometry args={[1, 32, 32]} />
       <meshStandardMaterial color="#ffffff" />
      </mesh>
-     <mesh>
+     <mesh rotation={[ - (angleToRadians(90)), 0, 0]}>
       <planeGeometry args={[7, 7]} />
-      <meshStandardMaterial color="#ffffff" />
+      <meshStandardMaterial color ="#ffffff" />
      </mesh>
     </>
   )
