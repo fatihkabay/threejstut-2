53386e236c43f5dd490042499c1370bee58a5425
Fatih Kabay <mfthkabay@gmail.com>
2023-08-18 23:54:06 +0300

    added orbitRef

--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,16 +1,26 @@
 /* eslint-disable react/no-unknown-property */
 // eslint-disable-next-line no-unused-vars
-import { PerspectiveCamera } from "@react-three/drei"
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
 import { angleToRadians } from "../../utils/angle"
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
 
 export default function Three() {
+
+  const orbitControlsRef = useRef(null)
+
+  useFrame((state) => {
+     console.log(state.mouse)
+  }) 
+
   return (
     <>
 
-     <PerspectiveCamera makeDefault position={[0, 0, 3]}/>
+     <PerspectiveCamera makeDefault position={[0, 1, 5]}/>
+     <OrbitControls ref={orbitControlsRef} />
 
-     <mesh>
-      <sphereGeometry args={[1, 32, 32]} />
+     <mesh position={[0, 0.5, 0]}>
+      <sphereGeometry args={[0.5, 32, 32]} />
       <meshStandardMaterial color="#ffffff" />
      </mesh>
      <mesh rotation={[ - (angleToRadians(90)), 0, 0]}>
